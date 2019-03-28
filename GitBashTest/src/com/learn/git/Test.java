56d25c8156d23768a36a4d55c1f9bbd8aeee3a56
kkushalgupta <kkushal.gupta@gmail.com>
2019-03-28 23:09:10 +0530

    commit via egit eclipse plugin

--- GitBashTest/src/com/learn/git/Test.java
+++ GitBashTest/src/com/learn/git/Test.java
@@ -3,7 +3,7 @@ package com.learn.git;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("this is done via eclipse");
 
 	}
 
